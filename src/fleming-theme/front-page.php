5e5931b618b333127f339071b43e1cbb20476537
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2018-06-04 15:02:44 +0100

    Front page draft partially connected to database.

--- src/fleming-theme/front-page.php
+++ src/fleming-theme/front-page.php
@@ -1,6 +1,7 @@
 <?php
 
 include __DIR__ . '/php/get-css-filename.php';
+include 'query-utilities.php';
 
 /**
  * NOTE:
@@ -12,6 +13,24 @@ include __DIR__ . '/php/get-css-filename.php';
  * VIEWs are located in the ./templates folder and have a .html file extension
  */
 
+function toTimestamp($date) {
+    return DateTime::createFromFormat('!d/m/Y', $date)->getTimestamp();
+}
+ 
+function sort_opportunities($opportunities) {
+    usort($opportunities, function($a, $b) {
+        $aDate = get_field_objects($a->ID)['deadline']['value'];
+        $bDate = get_field_objects($b->ID)['deadline']['value'];
+        return toTimestamp($aDate) - toTimestamp($bDate);
+    });
+    return $opportunities;
+}
+
+function is_in_future($opportunity) {
+    $date = get_field_objects($opportunity->ID)['deadline']['value'];
+    if ($date == '') return false;
+    return (toTimestamp($date) - time()) >= 0;
+}
 
 function fleming_get_content() {
     $fleming_content = array(
@@ -19,6 +38,31 @@ function fleming_get_content() {
         "css_filename" => get_css_filename(),
         "fields" => get_field_objects()
     );
+
+    $opportunities = get_posts(array('post_type'=>'grants','numberposts'=>-1));
+    $opportunities = array_filter($opportunities, function($opportunity) {
+        return is_in_future($opportunity);
+    });
+    $opportunities = array_slice(sort_opportunities($opportunities), 0, 4);
+    $fleming_content["opportunities"] = $opportunities;
+
+    $fleming_content["fields"]["headline_case_study"] = get_post_title_and_fields(
+        $fleming_content["fields"]["headline_case_study"]["value"]->ID
+    );
+
+    $fleming_content["fields"]["headline_grant_type"] = get_post_title_and_fields(
+        $fleming_content["fields"]["headline_grant_type"]["value"]->ID
+    );
+
+    $fleming_content["fields"]["headline_project"] = get_post_title_and_fields(
+        $fleming_content["fields"]["headline_project"]["value"]->ID
+    );
+
+    foreach($fleming_content["opportunities"] as &$opportunity) {
+        $opportunity = get_post_title_and_fields(
+            $opportunity->ID
+        );
+    }
 
     return $fleming_content;
 }
