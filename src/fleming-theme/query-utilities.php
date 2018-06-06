043a386abb99a74fc3061e3004135ad0c1e6c521
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2018-06-06 14:39:43 +0100

    Minor data structure and function name changes.

--- src/fleming-theme/query-utilities.php
+++ src/fleming-theme/query-utilities.php
@@ -1,6 +1,6 @@
 <?php
 
-function get_post_title_and_fields($postID) {
+function get_post_data_and_fields($postID) {
     $result = array('data'=>get_post($postID), 'fields'=>get_field_objects($postID));
     $result['data']->guid = htmlspecialchars_decode($result['data']->guid);
     return $result;
@@ -9,7 +9,7 @@ function get_post_title_and_fields($postID) {
 function get_referring_posts($postID, $post_type, $reference_type) {
     $posts = get_posts(array('post_type'=>$post_type,'numberposts'=>-1));
     foreach($posts as &$post) {
-        $post['post_data'] = get_post_title_and_fields($post->ID);
+        $post['post_data'] = get_post_data_and_fields($post->ID);
     }
     $posts = array_filter($posts, function($post) use($postID, $reference_type) {
         return $post['post_data']['fields'][$reference_type]['value']->ID == $postID;
