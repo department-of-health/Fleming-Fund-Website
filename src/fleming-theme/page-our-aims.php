97c3f123b425467a6724e3e7c6c16a5ac5734a66
Tom-Softwire <thomas.frodsham@softwire.com>
2018-06-14 15:33:07 +0100

    Merge master

---
+++ src/fleming-theme/page-our-aims.php
@@ -0,0 +1,37 @@
+<?php
+
+include __DIR__ . '/php/get-css-filename.php';
+include 'query-utilities.php';
+include 'navigation/index.php';
+
+/**
+ * NOTE:
+ * 
+ * This is a CONTROLLER file.
+ * It generates an object containing content for the page.
+ * 
+ * You might also be interested in the VIEW.
+ * VIEWs are located in the ./templates folder and have a .html file extension
+ */
+
+function fleming_get_content() {
+    $fleming_content = array(
+        "title" => get_the_title(),
+        "css_filename" => get_css_filename(),
+        "fields" => get_field_objects(),
+        'nav' => get_nav_builder()->withMenuRoute('about', 'aims')->build()
+    );
+
+    $aims = get_posts(array('post_type'=>'aims','numberposts'=>-1));
+    foreach($aims as &$aim) {
+        $aim = get_post_data_and_fields($aim->ID);
+    }
+    $fleming_content['aims'] = $aims;
+    $fleming_content['aims_count'] = count($aims);
+
+    return $fleming_content;
+}
+
+
+$template_name = pathinfo(__FILE__)['filename'];
+include __DIR__ . '/use-templates.php';
--- src/fleming-theme/query-utilities.php
+++ src/fleming-theme/query-utilities.php
@@ -1,12 +1,25 @@
 <?php
 
-function get_post_title_and_fields($postID) {
+function get_post_data_and_fields($postID) {
     if (!isset($postID)) {
         return null;
     }
-    return array(
-        'title'=>get_raw_title($postID),
-        'post_name'=>get_post_field('post_name', $postID),
-        'fields'=>get_field_objects($postID)
-    );
+    $result = array('data'=>get_post($postID), 'fields'=>get_field_objects($postID));
+    $result['data']->page_title = get_raw_title($postID);
+    $result['data']->guid = htmlspecialchars_decode($result['data']->guid);
+    return $result;
+}
+
+function get_referring_posts($postID, $post_type, $reference_type) {
+    $posts = get_posts(array('post_type'=>$post_type,'numberposts'=>-1));
+    foreach($posts as &$post) {
+        $post['post_data'] = get_post_data_and_fields($post->ID);
+    }
+    $posts = array_filter($posts, function($post) use($postID, $reference_type) {
+        return $post['post_data']['fields'][$reference_type]['value']->ID == $postID;
+    });
+    foreach($posts as &$post) {
+        unset($post['post_data']);
+    }
+    return $posts;
 }
