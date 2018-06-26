1bf7b8cced78f1b34776a9d417500eb2d5b5f38b
Tom-Softwire <thomas.frodsham@softwire.com>
2018-06-26 16:42:44 +0100

    base projects page

---
+++ src/fleming-theme/page-projects.php
@@ -0,0 +1,36 @@
+<?php
+
+include __DIR__ . '/php/get-css-filename.php';
+include 'query-utilities.php';
+include 'navigation/index.php';
+
+/**
+ * NOTE:
+ * 
+ * This is a CONTROLLER file.
+ * It generates an object containing content for the page.
+ * 
+ * You might also be interested in the VIEW.
+ * VIEWs are located in the ./templates folder and have a .html file extension
+ */
+
+function fleming_get_content() {
+    $fleming_content = array(
+        "title" => get_raw_title(),
+        "css_filename" => get_css_filename(),
+        "fields" => get_field_objects(),
+        'nav' => get_nav_builder()->withMenuRoute('regions', 'projects')->build()
+    );
+
+    $allProjects = get_posts(array('post_type'=>'projects','numberposts'=>-1));
+    foreach($allProjects as &$project) {
+        $project = get_post_data_and_fields($project->ID);
+    }
+    $fleming_content["allProjects"] = $allProjects;
+
+    return $fleming_content;
+}
+
+
+$template_name = pathinfo(__FILE__)['filename'];
+include __DIR__ . '/use-templates.php';
