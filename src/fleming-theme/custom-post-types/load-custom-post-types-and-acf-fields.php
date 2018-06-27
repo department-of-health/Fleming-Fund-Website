5d65b62389eda9e3aeebb378a4b27e28d613e9c2
Tom-Softwire <thomas.frodsham@softwire.com>
2018-06-27 17:23:50 +0100

    fixed custom post types

--- src/fleming-theme/custom-post-types/load-custom-post-types-and-acf-fields.php
+++ src/fleming-theme/custom-post-types/load-custom-post-types-and-acf-fields.php
@@ -1,17 +1,31 @@
 <?php
 
-function load_custom_post_types_and_acf_fields() {
+function init_custom_post_types() {
+    include __DIR__ . '/types/aims.php';
+    include __DIR__ . '/types/countries.php';
+    include __DIR__ . '/types/disciplines.php';
+    include __DIR__ . '/types/events.php';
+    include __DIR__ . '/types/grant-types.php';
+    include __DIR__ . '/types/grants.php';
+    include __DIR__ . '/types/organisation-types.php';
+    include __DIR__ . '/types/organisations.php';
+    include __DIR__ . '/types/people.php';
+    include __DIR__ . '/types/projects.php';
+    include __DIR__ . '/types/publication-types.php';
+    include __DIR__ . '/types/publications.php';
+    include __DIR__ . '/types/regions.php';
+    include __DIR__ . '/types/topics.php';
+}
+add_action('init', 'init_custom_post_types');
 
-    // Pages (ACF settings)
+function init_acf_fields() {
+    $load_flexible_content = true;
+
+    load_acf_fields('types/front-page.json');
 
     $pagesWithFlexibleContent = [
         'about-us',
     ];
-
-    load_acf_fields('types/front-page.json');
-
-    $load_flexible_content = true;
-
     $pageData = get_post(is_admin() ? ($_GET['post'] ?? $_POST['post_ID']) : null);
     if (isset($pageData)) {
         if ($pageData->post_type === 'page') {
@@ -25,56 +39,25 @@ function load_custom_post_types_and_acf_fields() {
         }
     }
 
-    // Custom post types (type definition + optional ACF settings)
-
-    include __DIR__ . '/types/aims.php';
     load_acf_fields('types/aims.json');
-
-    include __DIR__ . '/types/countries.php';
     load_acf_fields('types/countries.json');
-
-    include __DIR__ . '/types/disciplines.php';
-
-    include __DIR__ . '/types/events.php';
     load_acf_fields('types/events.json');
-
-    include __DIR__ . '/types/grant-types.php';
     load_acf_fields('types/grant-types.json');
-
-    include __DIR__ . '/types/grants.php';
     load_acf_fields('types/grants.json');
-
-    include __DIR__ . '/types/organisation-types.php';
     load_acf_fields('types/organisation-types.json');
-
-    include __DIR__ . '/types/organisations.php';
     load_acf_fields('types/organisations.json');
-
-    include __DIR__ . '/types/people.php';
     load_acf_fields('types/people.json');
-
-    include __DIR__ . '/types/projects.php';
     load_acf_fields('types/projects.json');
-
-    include __DIR__ . '/types/publication-types.php';
-
-    include __DIR__ . '/types/publications.php';
     load_acf_fields('types/publications.json');
-
-    include __DIR__ . '/types/regions.php';
     load_acf_fields('types/regions.json');
-
-    include __DIR__ . '/types/topics.php';
-
     if ($load_flexible_content) {
         load_acf_fields('types/flexible-content.json');
     }
-
 }
 if (is_admin()) {
-    add_action('acf/init', 'load_custom_post_types_and_acf_fields');
+    add_action('acf/init', 'init_acf_fields');
 } else {
-    add_action('wp', 'load_custom_post_types_and_acf_fields');
+    add_action('wp', 'init_acf_fields');
 }
 
 function load_acf_fields($filename) {
