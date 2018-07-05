f4918726808ea8ad46a15ef434ab672f2b18420f
Rupert Wood <rupert.wood@softwire.com>
2018-07-05 11:54:57 +0100

    General page from flexible content
    Doesn't yet have menu selection based on URL

--- src/fleming-theme/page.php
+++ src/fleming-theme/page.php
@@ -1,24 +1,28 @@
 <?php
 
 require_once __DIR__ . '/php/get-css-filename.php';
+require_once 'navigation/index.php';
 
 /**
  * NOTE:
- * 
+ *
  * This is a CONTROLLER file.
  * It generates an object containing content for the page.
- * 
+ *
  * You might also be interested in the VIEW.
  * VIEWs are located in the ./templates folder and have a .html file extension
  */
 
-
 function fleming_get_content() {
     $fleming_content = array(
         "title" => get_raw_title(),
-        "css_filename" => get_css_filename(),
-        "fields" => get_field_objects()
+        "fields" => get_field_objects(),
+        "nav" => get_nav_builder()
+            ->build()
     );
+    // need ->withMenuRoute(...) based on the current URL
+
+    process_flexible_content($fleming_content, $fleming_content['fields']['flexible_content']);
 
     return $fleming_content;
 }
