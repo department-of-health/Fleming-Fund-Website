58b95e5e0ad061ed432070cb6378d268d6180584
Rupert Wood <rupert.wood@softwire.com>
2018-07-17 15:01:47 +0100

    Don't show partner images in footer when light
    For now implemented by not loading them, and not e.g. linking to the
    partners page instead. (Which we might want to do in the general
    case.)

--- src/fleming-theme/use-templates.php
+++ src/fleming-theme/use-templates.php
@@ -5,14 +5,16 @@ require_once 'twig.php';
 require_once __DIR__ . '/php/get-css-filename.php';
 require_once 'navigation/index.php';
 
+$light = isset($_COOKIE['low-bandwidth']);
+
 $content = fleming_get_content();
 $content['css_filename'] = $content['css_filename'] ?? get_css_filename();
 $content['js_bundle_filename'] = $content['js_bundle_filename'] ?? get_js_bundle_filename();
 $content["nav"] = $content["nav"] ?? get_home_nav();
 $content["fields"] = $content["fields"] ?? get_field_objects();
-$content['weight'] = isset($_COOKIE['low-bandwidth']) ? 'light' : 'heavy';
+$content['weight'] = $light ? 'light' : 'heavy';
 $content['current_uri'] = $_SERVER['REQUEST_URI'];
-$content['footer_organisations'] = get_footer_organisations();
+$content['footer_organisations'] = $light ? null : get_footer_organisations();
 
 if (isset($_GET["json"])) {
     header('Content-Type: application/json');
