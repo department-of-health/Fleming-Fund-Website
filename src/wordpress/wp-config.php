070596bdfa0448d1ed2b9b3d6f4565a050f32dab
Rupert Wood <rupert.wood@softwire.com>
2018-08-21 18:59:20 +0100

    CloudFront SSL and separate origin fixes

--- src/wordpress/wp-config.php
+++ src/wordpress/wp-config.php
@@ -1,4 +1,16 @@
 <?php
+//Begin Really Simple SSL Load balancing fix
+if ((isset($_ENV["HTTPS"]) && ("on" == $_ENV["HTTPS"]))
+    || (isset($_SERVER["HTTP_X_FORWARDED_SSL"]) && (strpos($_SERVER["HTTP_X_FORWARDED_SSL"], "1") !== false))
+    || (isset($_SERVER["HTTP_X_FORWARDED_SSL"]) && (strpos($_SERVER["HTTP_X_FORWARDED_SSL"], "on") !== false))
+    || (isset($_SERVER["HTTP_CF_VISITOR"]) && (strpos($_SERVER["HTTP_CF_VISITOR"], "https") !== false))
+    || (isset($_SERVER["HTTP_CLOUDFRONT_FORWARDED_PROTO"]) && (strpos($_SERVER["HTTP_CLOUDFRONT_FORWARDED_PROTO"], "https") !== false))
+    || (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && (strpos($_SERVER["HTTP_X_FORWARDED_PROTO"], "https") !== false))
+) {
+    $_SERVER["HTTPS"] = "on";
+}
+//END Really Simple SSL
+
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_NAME', $_ENV['DB_NAME']);
 
@@ -18,6 +30,17 @@ define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
 define('LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
 define('NONCE_SALT',       $_ENV['NONCE_SALT']);
 
+// Log request properties to the error log for debugging
+// error_log('wp_config HTTP_HOST='.$_SERVER["HTTP_HOST"].' HTTP_X_AMZ_CF_ID='.$_SERVER["HTTP_X_AMZ_CF_ID"]);
+
+if (isset($_SERVER["HTTP_X_AMZ_CF_ID"])) {
+  # From CloudFront
+  $_SERVER["HTTP_HOST"] = 'dev.flemingfund.org';
+} elseif (isset($_SERVER["HTTP_HOST"]) && ("origin.flemingfund.org" == $_SERVER["HTTP_HOST"])) {
+  # Direct access to origin
+  define('WP_HOME', 'https://origin.flemingfund.org/');
+  define('WP_SITEURL', 'https://origin.flemingfund.org/');
+}
 
 $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
